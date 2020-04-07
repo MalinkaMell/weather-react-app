9e6271de38274921d7f635eda878e81061fee49f
Maimell <malinka77@gmail.com>
2020-04-07 09:46:12 -0700

    Updates

---
+++ precache-manifest.87613b4118c1eae4329973a9510519bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6561b57a76b582f087c100fb2147b0",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "298b63efd3569130296a",
+    "url": "/weather-react-app/static/css/main.e678fa51.chunk.css"
+  },
+  {
+    "revision": "9f9313596d9db2f6bf09",
+    "url": "/weather-react-app/static/js/2.5657c9bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.5657c9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298b63efd3569130296a",
+    "url": "/weather-react-app/static/js/main.83acbc0a.chunk.js"
+  },
+  {
+    "revision": "2b976758fce97592be35",
+    "url": "/weather-react-app/static/js/runtime-main.02fa00f3.js"
+  }
+]);
