14000ed359c191b8c400a09b06a3005fa110f2a4
Maimell <malinka77@gmail.com>
2020-04-07 16:53:46 -0700

    added router, 5 days weather component, changed dates to 2020

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import weatherData from './data.json';
 import Body from './components/body/Body';
 import Results from './components/results/Results';
 import SearchContainer from './components/search/Search';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FiveDaysResults from './components/results/FiveDays';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,31 +16,31 @@ class App extends React.Component {
       data: [{
         "name": "Chicago",
         "forecast": [{
-          "Date": "04/05/2019",
+          "Date": "04/05/2020",
           "Time": "2.59pm",
           "temprature": 47,
           "feels": 40
         },
         {
-          "Date": "04/06/2019",
+          "Date": "04/06/2020",
           "Time": "2.59pm",
           "temprature": 57,
           "feels": 55
         },
         {
-          "Date": "04/07/2019",
+          "Date": "04/07/2020",
           "Time": "2.59pm",
           "temprature": 45,
           "feels": 44
         },
         {
-          "Date": "04/08/2019",
+          "Date": "04/08/2020",
           "Time": "2.59pm",
           "temprature": 61,
           "feels": 50
         },
         {
-          "Date": "04/09/2019",
+          "Date": "04/09/2020",
           "Time": "2.59pm",
           "temprature": 68,
           "feels": 65
@@ -62,10 +64,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <Body>
-        <SearchContainer handleChange={this.handleChange} handleSubmit={this.handleSubmit} input={this.state.input} />
-        {this.state.display !== null && <Results city={this.state.display} />}
-      </Body>
+      <Router>
+        <Body>
+          <Switch>
+            <Route exact path="/">
+              <SearchContainer handleChange={this.handleChange} handleSubmit={this.handleSubmit} input={this.state.input} />
+              {this.state.display !== null && <Results city={this.state.display} />}
+            </Route>
+            <Route path="/fivedays">
+              <FiveDaysResults city={this.state.display} />
+            </Route>
+          </Switch>
+        </Body>
+      </Router>
     )
   }
 }
--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,23 +1,32 @@
 import React from 'react';
-
+import Button from '../button/Button';
+import { Link } from "react-router-dom";
 
 const Results = props => {
+
   return (
     <div className="px-2 text-center">
       <h3 className="my-3">{props.city.name}</h3>
       <ul className="list-inline d-flex">
         {
-          props.city.forecast.map((item, i) => (
-            <li className="list-inline-item flex-fill text-center results p-2" key={i}>
-              <p>{item.Date}</p>
-              <p>{item.Time}</p>
-              <p>Temperature: {item.temprature}</p>
-              <p>{item.temprature >= 60 ? <i className="fas fa-sun fa-2x"></i> : <i className="fas fa-cloud fa-2x"></i>}</p>
-              <p>Feels: {item.feels}</p>
-            </li>
-          ))
+          props.city.forecast.map((item, i) => {
+            const date = new Date(item.Date).toDateString();
+            const today = new Date().toDateString();
+            if (date === today) {
+              return (
+                <li className="list-inline-item flex-fill text-center results p-2" key={i}>
+                  <p>{date}</p>
+                  <p>{item.Time}</p>
+                  <p>Temperature: {item.temprature}</p>
+                  <p>{item.temprature >= 60 ? <i className="fas fa-sun fa-2x"></i> : <i className="fas fa-cloud fa-2x"></i>}</p>
+                  <p>Feels: {item.feels}</p>
+                </li>
+              )
+            }
+          })
         }
       </ul>
+      <Link to="/fivedays"><Button type="button" btnValue="5 Day Weather" class="btn btn-primary my-4" /></Link>
     </div>
   )
 }
